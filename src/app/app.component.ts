158949d9e771d700f6fb03735586332416286f37
hariompatil <hariompatil757@gmail.com>
2025-02-18 23:10:22 +0530

    estore-6: category naviagtion bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { CatnavigationComponent } from "./catnavigation/catnavigation.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent,HeaderComponent,FontAwesomeModule],
+  imports: [RouterOutlet, HeaderComponent, HeaderComponent, FontAwesomeModule, CatnavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
