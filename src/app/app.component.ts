3f132b2d098d668ebe6b57ef9dd49fb7fe851798
hariompatil <hariompatil757@gmail.com>
2025-02-18 22:24:57 +0530

    estore-3:adding search br

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent,HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent,HeaderComponent,FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+ faSearch = faSearch;
 }
