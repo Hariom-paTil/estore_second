d3e58ee4ad0dcf52c867f5cccbd8bf54a1142080
hariompatil <hariompatil757@gmail.com>
2025-02-18 23:54:38 +0530

    estore-9:creating 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { Error404Component } from './error404/error404.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent}, 
-    { path: '', redirectTo: '/home', pathMatch: 'full' }];
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '**', component: Error404Component}
+];
