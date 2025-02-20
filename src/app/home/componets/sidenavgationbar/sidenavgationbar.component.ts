ab66bb79044757ff8011ea4f7cdec0b568ba1ccf
hariompatil <hariompatil757@gmail.com>
2025-02-20 20:52:57 +0530

    estore-11:fetching data into side navigation compnent

--- src/app/home/componets/sidenavgationbar/sidenavgationbar.component.ts
+++ src/app/home/componets/sidenavgationbar/sidenavgationbar.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { Category } from '../../types/category.type';
+import { CategoryService } from '../../services/category.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidenavgationbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidenavgationbar.component.html',
   styleUrl: './sidenavgationbar.component.scss'
 })
 export class SidenavgationbarComponent {
+  categories: Category[] = [];
+  constructor(categoryService: CategoryService) {
+    this.categories = categoryService.getAllCategories();
+  }
 
+  getCategories(parentCategoryId?: number): Category[] {
+    return this.categories.filter(
+      (category) => category.parent_category_id === parentCategoryId
+    );
+  }
 }
