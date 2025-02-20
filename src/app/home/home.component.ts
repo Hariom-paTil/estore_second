31900888e6690f28a71ccc669436b988f4ec6c21
hariompatil <hariompatil757@gmail.com>
2025-02-20 21:59:25 +0530

    estore-13:styling product card

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./componets/header/header.component";
 import { CatnavigationComponent } from "./componets/catnavigation/catnavigation.component";
 import { SidenavgationbarComponent } from "./componets/sidenavgationbar/sidenavgationbar.component";
+import { ProductsComponent } from "./componets/products/products.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, CatnavigationComponent, SidenavgationbarComponent],
+  imports: [HeaderComponent, CatnavigationComponent, SidenavgationbarComponent, ProductsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
