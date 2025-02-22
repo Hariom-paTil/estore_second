446ad4c606f55531a5c3f3d0364665765d77a730
hariompatil <hariompatil757@gmail.com>
2025-02-22 21:21:31 +0530

    estore_server-4:connect to my sql

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql2');
 const PORT = 5001;
 
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'estore',
+  port: 3306,
+  multipleStatements: true,
+});
+
 app.get('/', (req, res) => {
-  let prodData = {
-    pName: 'Shirts',
-    price: 45,
-    img: 'shop-1.jpg',
-  };
-  res.status(200).send(prodData);
+  pool.getConnection((err, connection) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send('Connection Established');
+    }
+  });
 });
 
 const server = app.listen(PORT, () => {
