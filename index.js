a17650aefa4a05c3fe1cb1d3cfd1782189785d25
hariompatil <hariompatil757@gmail.com>
2025-02-22 21:14:57 +0530

    estore_server-2:Configuring API Route

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = 5001;
 
+app.get('/', (req, res) => {
+  let prodData = {
+    pName: 'Shirts',
+    price: 45,
+    img: 'shop-1.jpg',
+  };
+  res.status(200).send(prodData);
+});
+
 const server = app.listen(PORT, () => {
-    console.log('App is running on the port - 5001');
+  console.log('App is running on the port - 5001');
 });
