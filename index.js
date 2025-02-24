b738ca4caa27aa43f5137dd0ca582e851ad8e38b
hariompatil <hariompatil757@gmail.com>
2025-02-24 22:54:55 +0530

    estore_server-8:Fetching Categories from server

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const productCategories = require('./routes/product_category');
 const app = express();
-
+const cors = require('cors');
 const PORT = 5001;
 
+app.use(cors());
 app.use('/productCategories',productCategories);
 
 const server = app.listen(PORT, () => {
