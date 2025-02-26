c87fcacf83ec1dae266a5cb1cfa5ed143e93a0ae
hariompatil <hariompatil757@gmail.com>
2025-02-26 13:41:51 +0530

    estore-17:Fetching Categories in Category navigation bar

--- src/app/home/componets/catnavigation/catnavigation.component.ts
+++ src/app/home/componets/catnavigation/catnavigation.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-
+import { CategoryService } from '../../services/category.service';
+import { Category } from '../../types/category.type';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-catnavigation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './catnavigation.component.html',
   styleUrl: './catnavigation.component.scss'
 })
 export class CatnavigationComponent {
+  categories: Category[] = [];
 
+  constructor(categoryService: CategoryService) {
+    categoryService.getAllCategories().subscribe((categories) => {
+      this.categories = categories.filter(category => category.parent_category_id === null);
+    });
+  }
 }
