d33b3ef03d6dcb0c2ea9a83a285830e656b61aa6
hariompatil <hariompatil757@gmail.com>
2025-02-27 15:24:05 +0530

    estore_server:-10:Query parameters to Filter products by category

--- routes/product.js
+++ routes/product.js
@@ -4,24 +4,36 @@ const pool =require('../shared/pool');
 
 
 products.get('/', (req, res) => {
-    pool.query('select * from products', (error, products) => {
-      if (error) {
-        res.status(500).send(error);
-      } else {
-        res.status(200).send(products);
-      }
-    });
+  var mainCategoryId = req.query.maincategoryid;
+  var subCategoryId = req.query.subcategoryid;
+
+  let query = 'select * from products';
+  if (subCategoryId) {
+    query += ' where category_id = ' + subCategoryId;
+  }
+
+  if (mainCategoryId) {
+    query = `select products.* from products, categories 
+    where products.category_id = categories.id and categories.parent_category_id = ${mainCategoryId}`;
+  }
+  pool.query(query, (error, products) => {
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      res.status(200).send(products);
+    }
   });
-  
-  products.get('/(:id)', (req, res) => {
-    let id = req.params.id;
-    pool.query('select * from products where id = ' + id, (error, products) => {
-      if (error) {
-        res.status(500).send(error);
-      } else {
-        res.status(200).send(products);
-      }
-    });
+});
+
+products.get('/(:id)', (req, res) => {
+  let id = req.params.id;
+  pool.query('select * from products where id = ' + id, (error, products) => {
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      res.status(200).send(products);
+    }
   });
-  
-  module.exports = products;
+});
+
+module.exports = products;
