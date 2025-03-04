23de6fe5a7c1b7608d2d1905eae722adc4f6693a
hariompatil <hariompatil757@gmail.com>
2025-03-04 16:08:01 +0530

    estore_server:-11:Adding keywords in database

--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const products = express.Router();
-const pool =require('../shared/pool');
-
+const pool = require('../shared/pool');
 
 products.get('/', (req, res) => {
   var mainCategoryId = req.query.maincategoryid;
   var subCategoryId = req.query.subcategoryid;
+  var keyword = req.query.keyword;
 
   let query = 'select * from products';
   if (subCategoryId) {
@@ -16,6 +16,11 @@ products.get('/', (req, res) => {
     query = `select products.* from products, categories 
     where products.category_id = categories.id and categories.parent_category_id = ${mainCategoryId}`;
   }
+
+  if (keyword) {
+    query += ` and keywords like '%${keyword}%'`;
+  }
+
   pool.query(query, (error, products) => {
     if (error) {
       res.status(500).send(error);
@@ -37,3 +42,4 @@ products.get('/(:id)', (req, res) => {
 });
 
 module.exports = products;
+
