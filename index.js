d5a673e6082a7a77e6f6834d51d7d0734bd8f946
hariompatil <hariompatil757@gmail.com>
2025-03-09 13:01:56 +0530

    estore_server:-12: User Signup API

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require('express');
 const productCategories = require('./routes/product_category');
 const app = express();
 const cors = require('cors');
-const PORT = 5001;
+const PORT = 5003;
+const users = require('./routes/user');
 const products = require('./routes/product')
-
+const bodyparser = require('body-parser');
 
 app.use(cors());
+app.use(bodyparser.json());
 app.use('/productCategories',productCategories);
 app.use('/products',products);
+app.use('/users',users);
 const server = app.listen(PORT, () => {
-  console.log('App is running on the port - 5001');
+  console.log('App is running on the port - 5002');
 });
