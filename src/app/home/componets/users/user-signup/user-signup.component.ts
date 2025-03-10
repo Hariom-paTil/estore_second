55cb312fce434c48eec33ee93e2b631526759883
hariompatil <hariompatil757@gmail.com>
2025-03-10 15:37:37 +0530

    estore-35:Adding validations in user sign up form

--- src/app/home/componets/users/user-signup/user-signup.component.ts
+++ src/app/home/componets/users/user-signup/user-signup.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component , OnInit} from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { matchPasswords } from '../validators/match_password';
 
 @Component({
   selector: 'app-user-signup',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './user-signup.component.html',
   styleUrl: './user-signup.component.scss'
 })
@@ -12,17 +14,38 @@ export class UserSignupComponent implements OnInit {
   userSignupForm: FormGroup;
   constructor(private fb: FormBuilder) {}
   ngOnInit(): void {
-    this.userSignupForm = this.fb.group({
-      firstName: [''],
-      lastName: [''],
-      address: [''],
-      city: [''],
-      state: [''],
-      pin: [''],
-      email: [''],
-      password: [''],
-      confirmPassword: [''],
-    });
+    this.userSignupForm = this.fb.group(
+      {
+        firstName: ['', Validators.required],
+        lastName: [''],
+        address: [''],
+        city: [''],
+        state: [''],
+        pin: [''],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required],
+        confirmPassword: ['', Validators.required],
+      },
+      {
+        validator: matchPasswords,
+      }
+    );
+  }
+
+  get firstName(): AbstractControl<any, any> | null {
+    return this.userSignupForm.get('firstName');
+  }
+
+  get email(): AbstractControl<any, any> | null {
+    return this.userSignupForm.get('email');
+  }
+
+  get password(): AbstractControl<any, any> | null {
+    return this.userSignupForm.get('password');
+  }
+
+  get confirmPassword(): AbstractControl<any, any> | null {
+    return this.userSignupForm.get('confirmPassword');
   }
 
   onSubmit(): void {}
