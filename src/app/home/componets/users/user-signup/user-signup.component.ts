dee16c83a1c804ca03a17b3a3625b53828ad1791
hariompatil <hariompatil757@gmail.com>
2025-03-10 15:53:44 +0530

    estore-36:Connect sign up form to API

--- src/app/home/componets/users/user-signup/user-signup.component.ts
+++ src/app/home/componets/users/user-signup/user-signup.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component , OnInit} from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { matchPasswords } from '../validators/match_password';
+import { UsersServicesService } from '../users-services.service';
+import { user } from '../../../types/user.type';
 
 @Component({
   selector: 'app-user-signup',
@@ -12,7 +14,11 @@ import { matchPasswords } from '../validators/match_password';
 })
 export class UserSignupComponent implements OnInit {
   userSignupForm: FormGroup;
-  constructor(private fb: FormBuilder) {}
+  alertMessage: string = '';
+  alertType: number = 0; // 0-success, 1-warning, 2-error
+
+  constructor(private fb: FormBuilder, private userService: UsersServicesService) {}
+
   ngOnInit(): void {
     this.userSignupForm = this.fb.group(
       {
@@ -48,5 +54,31 @@ export class UserSignupComponent implements OnInit {
     return this.userSignupForm.get('confirmPassword');
   }
 
-  onSubmit(): void {}
+  onSubmit(): void {
+    const user: user = {
+      firstName: this.firstName?.value,
+      lastName: this.userSignupForm.get('lastName')?.value,
+      address: this.userSignupForm.get('address')?.value,
+      city: this.userSignupForm.get('city')?.value,
+      state: this.userSignupForm.get('state')?.value,
+      pin: this.userSignupForm.get('pin')?.value,
+      email: this.userSignupForm.get('email')?.value,
+      password: this.userSignupForm.get('password')?.value,
+    };
+    this.userService.createUser(user).subscribe({
+      next: (result) => {
+        if (result.message === 'success') {
+          this.alertMessage = 'User created successfully';
+          this.alertType = 0;
+        } else if (result.message === 'Email already exists') {
+          this.alertMessage = result.message;
+          this.alertType = 1;
+        }
+      },
+      error: (error) => {
+        this.alertMessage = error.message;
+        this.alertType = 2;
+      },
+    });
+  }
 }
