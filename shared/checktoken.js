10a1f696b1776a8473e0e1b98ccdfaf2eb06ed82
hariompatil <hariompatil757@gmail.com>
2025-03-15 21:33:45 +0530

    estore_server:-20:login form not working problem slove

--- shared/checktoken.js
+++ shared/checktoken.js
@@ -6,6 +6,7 @@ module.exports = (req, res, next) => {
     jwt.verify(token, 'estore-secret-key');
     next();
   } catch (error) {
+    console.log('check token failed');
     res.status(401).send({ message: 'Authorization failed!' });
   }
 };
